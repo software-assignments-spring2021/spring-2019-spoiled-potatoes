30c84c070003c01f87f1c62ec3b987ac131c2d91
sam-gaylert <ssg413@nyu.edu>
2019-03-04 21:35:49 -0500

    implemented testing on Trader and TraderRepository(test), some failing and not yet resolved

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const { Trader } = require('./public/trader/trader.js');
 
 const app = express();
 
--- test/trader_repo_test.js
+++ test/trader_repo_test.js
@@ -11,7 +11,7 @@ describe('post()', () => {
     const password = '1234';
 
     // 2. ACT
-    const registered = traderRepo.post(username, password, email);
+    const registered = traderRepo.post(email, username, password);
 
     // 3. ASSERT
     expect(registered).to.be.equal(true);
@@ -31,5 +31,21 @@ describe('findUser()', () => {
     // 3. ASSERT
     expect(usr).to.not.be.equal(false);
     expect(usr.username).to.be.equal(username);
+  });
+});
+
+describe('addPortfolioItem()', () => {
+  it('should update an existing user portfolio and return it', () => {
+    const username = 'sam';
+    const email = 'email@email.com';
+    const password = '1234';
+
+    traderRepo.post(email, username, password);
+
+    const portfolio = traderRepo.addPortfolioItem(username, { asset: 1000 });
+
+    expect(portfolio[0]).to.be.equal(true);
+    expect(portfolio[0]).to.not.be.equal(false);
+    expect(portfolio[1].asset).to.be.equal(1000);
   });
 });
