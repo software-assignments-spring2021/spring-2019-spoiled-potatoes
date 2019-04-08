06b8ce720806e0ecb15f70219b44afc1764de237
sam-gaylert <ssg413@nyu.edu>
2019-04-08 17:22:11 -0400

    finishing last.fm search, working on DB search

--- client/src/AlbumSearch.js
+++ client/src/AlbumSearch.js
@@ -4,67 +4,78 @@ import axios from 'axios';
 import './App.css';
 // import App from './App'
 
-const apiKey = process.env.REACT_ALBUM_API_KEY;
+
 
 class AlbumSearch extends Component {
   constructor(props) {
     super(props)
-    this.state = {  name: "", artist:"" }
+    this.apiKey = process.env.REACT_APP_ALBUM_API_KEY;
+    this.state = {  name: "", artist:"",results:[] }
     this.handleAlbumSearch = this.handleAlbumSearch.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.searchDB = this.searchDB.bind(this);
   }
   
+
+  searchDB(mid, artist, album){
+    const postObj = {
+      mid: mid,
+      artist: artist,
+      album: album
+    }
+
+    axios.post("");
+  }
   
 
   handleAlbumSearch(event, inputName, inputArtist) {
     event.preventDefault();
 
-    let searchMethod = "";
     let paramObj ={};
-    /*
-    support 3 different methods:
 
-    - artist.gettopalbums     to search for albums from an artist
-        paramObj = {
-            method: searchMethod,
-            artist: inputArtist,
-            key: apiKey,
-            format: "json",
-        }
-    - album.getinfo           to search for just an album using artist name and album name
-        paramObj = {
-            method: searchMethod,
-            key: apiKey,
-            artist: inputArtist,
-            album: inputName,
-            format: "json",
-        }
-    - album.search            to free search an album just by name
-        paramObj = {
-            method: searchMethod,
-            album: inputName,
-            key: apiKey,
-            format: "json",
-        }
-    
-    method var will be reassigned based on parameters input by user
-
-    */
-    switch(inputName, inputArtist){
-        
+    if (inputName && inputArtist) {
+      paramObj = {
+        method: "album.getinfo",
+        api_key: this.apiKey,
+        artist: inputArtist,
+        album: inputName,
+        format: "json",
+      }
+    } else if (inputArtist) {
+      paramObj = {
+        method: "artist.gettopalbums",
+        artist: inputArtist,
+        api_key: this.apiKey,
+        format: "json",
     }
+    } else {
+      paramObj = {
+        method: "album.search",
+        album: inputName,
+        api_key: this.apiKey,
+        format: "json",
+      }
+    }
+    console.log(paramObj);
     axios
       .get('http://ws.audioscrobbler.com/2.0/', {
         params: paramObj
       })
       .then(response => {
-        console.log('login response: ')
-        console.log(response)
         if (response.status === 200) {
-          console.log(response)
           // update App.js state
+          if (paramObj['method'] == "album.getinfo"){
+            this.setState({
+              results: [<li>{response.data.album.name}</li>]
+            });
+          } else  {
+            this.setState({
+              results: response.data.topalbums.album.map(item => <li>{item['name']}</li>)
+            });
+          }
         }
       }).catch(error => {
-        console.log('login error: ')
+        console.log('album search error: ');
         console.log(error);
 
       })
@@ -73,6 +84,7 @@ class AlbumSearch extends Component {
 
   render() {
     return (
+      <div>
       <form onSubmit={(event) => this.handleAlbumSearch(event, this.state.name, this.state.artist)}>
         <div class="form-group">
           <h1>Search for an album to start reviewing</h1>
@@ -82,9 +94,11 @@ class AlbumSearch extends Component {
             Artist:
           <input type="text" name="artist" value={this.state.artist} onChange={this.handleInputChange} />
           </label>
-          <input type="submit" value="Search" />
+          <input type="submit" disabled={!(this.state.name || this.state.artist)} value="Search" />
         </div>
       </form>
+      <ul>{this.state.results}</ul>
+      </div>
     );
   }
 
@@ -96,6 +110,7 @@ class AlbumSearch extends Component {
     this.setState({
       [name]: value
     });
+
   }
 }
 
