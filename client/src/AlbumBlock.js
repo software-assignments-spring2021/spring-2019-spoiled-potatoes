68eb0e443bce8fa297e3595aacb0587a624e8386
IcyX <powerjack227@gmail.com>
2019-04-17 23:47:59 -0400

    Simplified features and completed component

--- client/src/AlbumBlock.js
+++ client/src/AlbumBlock.js
@@ -2,31 +2,12 @@ import React, { Component } from 'react';
 import { Image,Button,Container,Row,Col } from 'react-bootstrap';
 // import logo from './logo.svg';
 import './App.css';
+//import DummyListComponent from "./DummyListComponent";
 // import axios from 'axios';
 
 
 
 class AlbumBlock extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-        name: "" , artist: "", img: ""
-    };
-    this.selectImage = this.selectImage.bind(this);
-  };
-
-  
-    selectImage(mbid, name){
-
-    }
-
-
-
-    componentDidMount(){
-      
-
-    }
 
 
     render() {
@@ -35,21 +16,26 @@ class AlbumBlock extends Component {
         <Container fluid>
             <Row>
                 <Col xs>
-                <Image src={this.state.img}/>
+                <Image src={this.props.image}/>
                 </Col>
                 <Col>
                 <h4> 
-                  {this.state.name}
+                  {this.props.name}
                 </h4>
                 <h5> 
-                  {this.state.artist}
+                  {this.props.artist}
                 </h5>
                 </Col>
             </Row>
             <Row>
-                <Col>
-                <Button variant="success">Upvote</Button>
-                <Button variant="danger">Downvote</Button>
+                <Col xs>
+                  
+                    <Button variant="success" size="sm"> Upvote </Button>
+              
+                    <Button variant="danger" size="sm">Downvote</Button>
+
+                    <h5> {this.props.vote} </h5>
+                
                 </Col>
             </Row>
         </Container>
