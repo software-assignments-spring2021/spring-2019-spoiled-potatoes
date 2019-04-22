34daeb4a2fa9746060de96a0284ecf795a3b8f1c
itcarrillo <itcarrillo@gmail.com>
2019-04-22 18:06:41 -0400

    Hiding comment submit button if state.text is ""

--- client/src/AddComment.js
+++ client/src/AddComment.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 // import logo from './logo.svg';
 import './App.css';
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 // import App from './App'
 
 class Login extends Component {
   constructor(props) {
     super(props)
-    this.state = { text: ""}
+    this.state = { text: "" }
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -20,9 +20,9 @@ class Login extends Component {
     console.log(this.props)
     console.log(this.props.username);
     console.log(this.state);
-    axios.post('/comment',{username:this.props.username, albumObjectId: this.props.album, text:this.state.text,}).then( response => {
+    axios.post('/comment', { username: this.props.username, albumObjectId: this.props.album, text: this.state.text, }).then(response => {
       console.log(response.data);
-      this.setState({text: ""});
+      this.setState({ text: "" });
       //this.props.parent.forceUpdate();
       window.location.reload();
     });
@@ -31,15 +31,15 @@ class Login extends Component {
 
   render() {
     return (
-        <Form
+      <Form
         onSubmit={e => this.handleSubmit(e)}
-        >
+      >
         <Form.Group controlId="commentForm">
           <Form.Label>Comment</Form.Label>
           <Form.Control name="text" required value={this.state.text} onChange={this.handleInputChange} as="textarea" rows="3" />
         </Form.Group>
-        <Button  variant="primary" type="submit">
-            Submit
+        <Button variant="primary" type="submit" hidden={!this.state.text}>
+          Submit
         </Button>
       </Form>
     );
