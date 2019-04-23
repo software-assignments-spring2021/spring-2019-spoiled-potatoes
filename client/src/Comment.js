a2fffb64f61b8f9711aea937fbce36ac5e765b6d
sam-gaylert <ssg413@nyu.edu>
2019-04-23 13:21:24 -0400

    making style changes

--- client/src/Comment.js
+++ client/src/Comment.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 // import logo from './logo.svg';
 import './App.css';
@@ -11,21 +12,12 @@ class Comment extends Component {
         <div className="album">
         <Container>
             <Row>
-            <Col>
-            <Jumbotron>
-                {this.props.comment.username}
-            </Jumbotron>
-            </Col>
-            <Col xs={8}>
-            <Jumbotron>
-                <div>
-                <p>
-                {this.props.comment.text}
-                </p>
-                </div>
-                <small>{this.props.comment.timestamp.split(':')[0].split('T')[0]} at {this.props.comment.timestamp.split(':')[0].split('T')[1]}:{this.props.comment.timestamp.split(':')[1]}</small>
-            </Jumbotron>
-            </Col>
+              <Col>
+              <div class="panel panel-default">
+                <div class="panel-heading">Panel Heading</div>
+                <div class="panel-body">Panel Content</div>
+              </div>
+              </Col>
             </Row>
         </Container>
         </div>
@@ -35,6 +27,30 @@ class Comment extends Component {
   
   export default Comment;
 
+/*
+<div class="comment-div">{this.props.comment.username}</div>
+            <div class="comment-div">
+                <p>
+                {this.props.comment.text}
+                </p>
+            </div>
+            <small>{this.props.comment.timestamp.split(':')[0].split('T')[0]} at 
+            {this.props.comment.timestamp.split(':')[0].split('T')[1]}:{this.props.comment.timestamp.split(':')[1]}</small>
 
 
+<Col xs={8}>
+            <Jumbotron className="comment-div">
+              <div >{this.props.comment.username}</div>
+            </Jumbotron>
+            <Jumbotron className="comment-div">
+                
+                <div>
+                <p>
+                {this.props.comment.text}
+                </p>
+                </div>
+                <small>{this.props.comment.timestamp.split(':')[0].split('T')[0]} at {this.props.comment.timestamp.split(':')[0].split('T')[1]}:{this.props.comment.timestamp.split(':')[1]}</small>
+            </Jumbotron>
+            </Col>
 
+*/
