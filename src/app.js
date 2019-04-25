9278b87f08f0fea6ff79f552b56eb1477f3fa475
itcarrillo <itcarrillo@gmail.com>
2019-04-25 13:31:23 -0400

    Serving build in production

--- src/app.js
+++ src/app.js
@@ -65,14 +65,9 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// serve build in production
 if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
 }
 
 computeTrending(Vote, Comment);
