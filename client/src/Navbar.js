e50a5bcc73a5d4539bbd118893c6bd7293280e32
itcarrillo <itcarrillo@gmail.com>
2019-05-02 13:21:03 -0400

    Adding favicon, removing logo from navbar #152

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -2,13 +2,11 @@
 import React, { Component } from 'react';
 // import Cookies from 'js-cookie';
 import './App.css';
-import PotatoHeader from './potatoHeader.png'
 
 class Navbar extends Component {
   render() {
     return (
       <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-        <img src={PotatoHeader} alt="a web icon for home button" style = {{ height: "70px", width:"80px"}}/>
         <a class="navbar-brand" href="/">Spoiled Potatoes</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
